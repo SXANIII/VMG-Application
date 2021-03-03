fceca6d6583328951db24ecd29d56e515c99f86a
SXANIII <39991127+SXANIII@users.noreply.github.com>
2021-03-03 22:19:58 +0200

    lol

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {MatCardModule} from '@angular/material/card';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { VehicleComponent } from './vehicle/vehicle.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import {MatButtonModule} from '@angular/material/button';
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatCardModule} from '@angular/material/card';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {FilterPipe} from './Pipes/filter.pipe';
+import { SortPipe } from './Pipes/sort.pipe'
+import {MatSelectModule} from '@angular/material/select';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    VehicleComponent
+    VehicleComponent,
+    FilterPipe,
+    SortPipe
   ],
   imports: [
     BrowserModule,
@@ -19,7 +31,13 @@ import {MatButtonModule} from '@angular/material/button';
     MatCardModule,
     AppRoutingModule,
     MatButtonModule,
-    HttpClientModule
+    HttpClientModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
