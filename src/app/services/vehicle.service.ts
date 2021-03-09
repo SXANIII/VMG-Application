9a03bc9f6f7e075ef4596b342362a2c0f6f07c6c
SXANIII <39991127+SXANIII@users.noreply.github.com>
2021-03-09 11:32:37 +0200

    Update vehicle.service.ts

--- src/app/services/vehicle.service.ts
+++ src/app/services/vehicle.service.ts
@@ -7,6 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class VehicleService {
 
+  // Perform a get request on given URL string and return result as any to be consumed
   readonly APIUrl = "https://capitalauto.co.za/api/v3/view_stock_complete_with_data?company_id=eq.635"
 
   constructor(private http : HttpClient) { }
